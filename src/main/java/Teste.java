ca29834c355e9f7272129fe44e265af51016cef7
rodrigo <rodrigo.almeida@jfive.com.br>
2012-12-21 17:34:54 -0200

    classe teste

---
+++ src/main/java/Teste.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: rodrigo
+ * Date: 12/21/12
+ * Time: 5:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Teste {
+}
