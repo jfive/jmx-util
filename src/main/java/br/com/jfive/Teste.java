8324eb6d89bc4f627a5cec69ae90c4d1ac664d10
rodrigo <rodrigo.almeida@jfive.com.br>
2012-12-21 18:05:22 -0200

    testes

---
+++ src/main/java/br/com/jfive/Teste.java
@@ -0,0 +1,24 @@
+package br.com.jfive;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rodrigo
+ * Date: 12/21/12
+ * Time: 5:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+
+        System.out.println("teste rodrigo");
+
+    }
+
+    public Date getDataAtual(){
+        return new Date();
+    }
+
+}
